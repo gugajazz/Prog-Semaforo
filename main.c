c5c25e7b082c227e9a033316ad9a1777d08186ec
Gustavo Poçaão <gugapocao@gmail.com>
2021-05-18 00:28:35 +0100

    legit o main gameplay já funciona

--- main.c
+++ main.c
@@ -37,22 +37,17 @@ void printTabuleiro(int ylenght ,int xlenght,int **tab){
     //printf("\n------------------------\n\n");
 }
 
-int changeTabuleiro(int xlenght ,int ylenght,int **tab, int xPosition, int yPosition){
-    if(xPosition>xlenght || yPosition>ylenght || xPosition<0 || yPosition<0){
-        printf("Posicao Invalida");
-        return 0;
-    }
-
-    if(tab[xPosition][yPosition]==0){
-        tab[xPosition][yPosition]=1;
+int changeTabuleiro(int **tabuleiro, int xPosition, int yPosition){
+    if(tabuleiro[xPosition][yPosition]==0){
+        tabuleiro[xPosition][yPosition]=1;
         return 1;
     }
-    else if(tab[xPosition][yPosition]==1){
-        tab[xPosition][yPosition]=2;
+    else if(tabuleiro[xPosition][yPosition]==1){
+        tabuleiro[xPosition][yPosition]=2;
         return 1;
     }
-    else if(tab[xPosition][yPosition]==2){
-        tab[xPosition][yPosition]=3;
+    else if(tabuleiro[xPosition][yPosition]==2){
+        tabuleiro[xPosition][yPosition]=3;
         return 1;
     }
     else{
@@ -61,92 +56,112 @@ int changeTabuleiro(int xlenght ,int ylenght,int **tab, int xPosition, int yPosi
     }
 }
 
-int checkForWinner(int xlenght ,int ylenght,int **tab){
+int checkForWinner(int xlenght ,int ylenght,int **tabuleiro, char *current_player){
     int equal_straight=0, last_equal;
     //horizontal
     for(int y=0;y<ylenght;y++){
-        if(tab[y][0]!=0){
-            last_equal=tab[y][0];
+        if(tabuleiro[y][0]!=0){
+            last_equal=tabuleiro[y][0];
 
             for(int x=1;x<xlenght;x++){
-            if(tab[y][x]==last_equal){
-                equal_straight++;
+                if(tabuleiro[y][x]==last_equal){
+                    equal_straight++;
+                }
+                //printf("x=%d y=%d val=%d las=%d strai=%d\n",x,y,tab[y][x],last_equal,equal_straight);
             }
-            printf("x=%d y=%d val=%d las=%d strai=%d\n",x,y,tab[y][x],last_equal,equal_straight);
-        }
-        printf("\n");
+            //printf("\n");
 
-        if(equal_straight==3){
-            printf("WINNER\n");
-            return 1;
-        }
-        else{
-            equal_straight=0;
-        }
+            if(equal_straight==3){
+                printTabuleiro(xlenght,ylenght,tabuleiro);
+                printf("PLAYER %c IS THE WINNER\n",*current_player);
+                return 0;
+            }
+            else{
+                equal_straight=0;
+            }
         }        
     }
 
     equal_straight=0;
     //vertical
     for(int x=0;x<xlenght;x++){
-        if(tab[0][x]!=0){
-            last_equal=tab[0][x];
-
+        if(tabuleiro[0][x]!=0){
+            last_equal=tabuleiro[0][x];
             for(int y=1;y<ylenght;y++){
-            if(tab[y][x]==last_equal){
-                equal_straight++;
+                if(tabuleiro[y][x]==last_equal){
+                    equal_straight++;
+                }
+                //printf("x=%d y=%d val=%d las=%d strai=%d\n",x,y,tab[y][x],last_equal,equal_straight);
             }
-            printf("x=%d y=%d val=%d las=%d strai=%d\n",x,y,tab[y][x],last_equal,equal_straight);
-        }
-        printf("\n");
+            printf("\n");
         
-        if(equal_straight==3){
-            printf("WINNER\n");
-            return 1;
-        }
-        else{
-            equal_straight=0;
-        }
+            if(equal_straight==3){
+                printTabuleiro(xlenght,ylenght,tabuleiro);
+                printf("PLAYER %c IS THE WINNER\n",*current_player);
+                return 0;
+            }
+            else{
+                equal_straight=0;
+            }
         }        
     }
 
     //diagonal esquerda p direita
-    if(tab[0][0]!=0){
+    if(tabuleiro[0][0]!=0){
         equal_straight=0;
-        last_equal=tab[0][0];
+        last_equal=tabuleiro[0][0];
         if(xlenght==ylenght){
             for(int i=1;i<xlenght;i++){
-                if(tab[i][i]==last_equal){
+                if(tabuleiro[i][i]==last_equal){
                     equal_straight++;
                 }
             }
             if(equal_straight==xlenght-1){
-                printf("WINNER");
+                printTabuleiro(xlenght,ylenght,tabuleiro);
+                printf("PLAYER %c IS THE WINNER\n",*current_player);
+                return 0;
             }
         }
     }
 
     //diagonal direita p esquerda
-    if(tab[0][xlenght-1]!=0){
+    if(tabuleiro[0][xlenght-1]!=0){
         equal_straight=0;
-        last_equal=tab[0][xlenght-1];
+        last_equal=tabuleiro[0][xlenght-1];
         if(xlenght==ylenght){
             for(int x=xlenght-2, y=1 ;x>=0; x--,y++){
-                if(tab[y][x]==last_equal){
+                if(tabuleiro[y][x]==last_equal){
                     equal_straight++;
                 }
             }
             if(equal_straight==xlenght-1){
-                printf("WINNER");
+                printTabuleiro(xlenght,ylenght,tabuleiro);
+                printf("PLAYER %c IS THE WINNER\n",*current_player);
             }
         }
     }
+
+    if(*current_player=='A'){
+        *current_player='B';
+    }
+    else{
+        *current_player='A';
+    }
+    return 1;
 }
 
-void GetInput(int *xPosition, int *yPosition, char current_player){
+int GetInput(int *xPosition, int *yPosition,char current_player, int **tabuleiro, int xlenght, int ylenght){
     printf("Vez do jogador %c\n",current_player);
-    printf("Escolha a posicao em que quer jogar (x y): ");
+    printf("Escolha a posicao em que quer jogar (y x): ");
     scanf("%d %d",xPosition,yPosition);
+
+    if(*xPosition>=xlenght || *yPosition>=ylenght || *xPosition<0 || *yPosition<0){ /*|| *xPosition<0 || *yPosition<0*/
+        printf("Posicao Invalida\n");
+        return 0;
+    }
+
+    changeTabuleiro(tabuleiro,*xPosition,*yPosition);
+    return 1;
 }
 
 void InicializaTabuleiro(int ylenght ,int xlenght,int **tab){
@@ -160,16 +175,25 @@ void InicializaTabuleiro(int ylenght ,int xlenght,int **tab){
     }
 }
 
-void ChangeTabuleiro(int *ylenght ,int *xlenght,int **tabuleiro, char mode){
+void CriaTabuleiro(int ylenght ,int xlenght,int **tabuleiro){
+    //TO BE CONTINUED
+}
+
+void ResizeTabuleiro(int *ylenght ,int *xlenght,int **tabuleiro, char mode){
     if(mode=='X'){
         //more xlenght
-        for ( size_t i = 0; i < (*ylenght); i++ )
-        {
-        int *tmp = realloc( tabuleiro[i], sizeof *tabuleiro[i] * ((*xlenght) + 1) );
-        if ( tmp )
-        {
-            tabuleiro[i] = tmp;
-        } 
+        for ( size_t i = 0; i < (*ylenght); i++ ){
+            int *tmp = realloc( tabuleiro[i], sizeof *tabuleiro[i] * ((*xlenght) + 1) );
+            if(tmp!=NULL){
+                tabuleiro[i] = tmp;
+            }
+            else{
+                printf("ERRO NA ALOCACAO DE MEMORIA");
+            } 
+        }
+        for(int i=0;i<(*ylenght);i++){
+            int j=(*xlenght);
+            tabuleiro[i][j]=0;
         }
         (*xlenght)++; 
     }
@@ -177,12 +201,16 @@ void ChangeTabuleiro(int *ylenght ,int *xlenght,int **tabuleiro, char mode){
     else if(mode=='Y'){
         //more ylenght
         int **tmp = realloc( tabuleiro, ((*ylenght)+1) );
-        if( tmp ){
+        if(tmp!=NULL){
             tabuleiro = tmp;
             tabuleiro[(*ylenght)] = malloc( sizeof(int) * ((*xlenght)+1) );
         }
         else{
-            printf("PUTAAA");
+            printf("ERRO NA ALOCACAO DE MEMORIA");
+        }
+        for(int j=0;j<(*xlenght);j++){
+            int i=(*ylenght);
+            tabuleiro[i][j]=0;
         }
         (*ylenght)++;
     }
@@ -193,12 +221,13 @@ void ChangeTabuleiro(int *ylenght ,int *xlenght,int **tabuleiro, char mode){
 }
 
 int main(){
+    //system("cls");
     int ylenght=4, xlenght=4;
     int **tabuleiro;
     char current_player='A';
     int playing = 1;
     int xPosition, yPosition;
-    
+
     tabuleiro = (int**)malloc(sizeof(int*)*ylenght);
     if (tabuleiro!=NULL){
         for (int i = 0; i<ylenght; i++){
@@ -209,11 +238,10 @@ int main(){
         printf("ERRO FODAO");
     }
     
-    InicializaTabuleiro(ylenght,xlenght,tabuleiro);
-    printTabuleiro(ylenght,xlenght,tabuleiro);
-    ChangeTabuleiro(&ylenght,&xlenght,tabuleiro,'X');
-    InicializaTabuleiro(ylenght,xlenght,tabuleiro);
-    printTabuleiro(ylenght,xlenght,tabuleiro);
+    //InicializaTabuleiro(ylenght,xlenght,tabuleiro);
+    //printTabuleiro(ylenght,xlenght,tabuleiro);
+    //ResizeTabuleiro(&ylenght,&xlenght,tabuleiro,'X');
+    //printTabuleiro(ylenght,xlenght,tabuleiro);
 
     //printTabuleiro(ylenght,xlenght,tabuleiro);
     //printTabuleiro(ylenght,xlenght,tabuleiro);
@@ -222,9 +250,11 @@ int main(){
     //checkForWinner(4,4,tabuleiro);
     //GetInput(&xPosition,&yPosition,current_player);
     //printf("%d %d",xPosition,yPosition,current_player);
-    /* while(playing){
-        printTabuleiro(4,4,tabuleiro);
 
-    } */
-
+    InicializaTabuleiro(ylenght,xlenght,tabuleiro);
+    while(playing){
+        printTabuleiro(xlenght,ylenght,tabuleiro);
+        GetInput(&xPosition,&yPosition,current_player,tabuleiro, xlenght, ylenght);
+        playing = checkForWinner(xlenght,ylenght,tabuleiro,&current_player);
+    }
 }
